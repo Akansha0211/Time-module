446ad9caa022a73f810efa4a6a075661c8b3f9a2
Akansha Saxena <49263297+Akansha0211@users.noreply.github.com>
2019-07-29 14:40:20 +0530

    Add files via upload

---
+++ Timee moduuleee.py
@@ -0,0 +1,21 @@
+import time
+# initial=time.time()
+# k=0
+# while(k<45):
+#     print("I love badminton",k)
+#     k+=1
+# print("While loop execution time",time.time()-initial,"seconds")
+# initial2=time.time()
+# for i in range(45):
+#     print("I love badminton",i)
+# print("For loop execution time",time.time()-initial2,"seconds")
+
+# local_time=time.asctime(time.localtime(time.time()))
+# print(local_time)
+#Mon Jul 29 14:28:36 2019
+
+# k=0
+# while(k<45):
+#     print("I love badminton",k)
+#     time.sleep(4)
+#     k+=1
